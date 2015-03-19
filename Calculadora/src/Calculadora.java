00eca09457ae65d549d14cd9cafe3cfd3e3c9c10
ikkg <flipas.garcia@gmail.com>
2015-03-19 18:36:19 +0100

    V1

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,26 @@
+
+public class Calculadora {
+	int num1, num2;
+	
+	public Calculadora(int num1, int num2){
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int resta(){
+		if(compruebaOrden()) return num1-num2;
+		else return num2-num1;
+		
+	}
+	
+	public boolean compruebaOrden(){
+		if(num1>=num2) return true;
+		return false;
+	}
+	
+	public int divide(){
+		if(num2==0) throw new java.lang.ArithmeticException("No vale");
+		return num1/num2;
+	}
+	
+}
